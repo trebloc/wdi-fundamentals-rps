ef5aacb8eb305648a79d934f056f0a9d57b1eb31
trebloc <ctse77@alum.berkeley.edu>
2015-11-08 15:20:18 -0800

    finish unit 3 homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,25 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    /* Your Expression */;
+    if (move === null || move === "") {
+        return getInput();
+    } else {
+        return move;
+    }
 }
+
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    if (move != null) {
+        return move;
+    } else if(move === "" || move === null) {
+        return randomPlay();    
+    }
+    
 }
 
 function getWinner(playerMove,computerMove) {
